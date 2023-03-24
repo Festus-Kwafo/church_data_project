8edf2fc6a9cf538e7e87dcd5cb352fb011f03222
Festus Kwafo Ofori <68119922+Festus-Kwafo@users.noreply.github.com>
2023-03-24 06:19:01 +0000

    docker setup for production

--- src/core/config.py
+++ src/core/config.py
@@ -2,9 +2,12 @@ from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
-class StaticStorage(S3Boto3Storage):
-    location = settings.STATICFILES_LOCATION
 
-    def __init__(self, *args, **kwargs):
-        kwargs['custom_domain'] = settings.AWS_S3_CUSTOM_DOMAIN
-        super(StaticStorage, self).__init__(*args, **kwargs)
+class StaticStorage(S3Boto3Storage):
+    location = 'static'
+    default_acl = 'public-read'
+
+class PublicMediaStorage(S3Boto3Storage):
+    location = 'media'
+    default_acl = 'public-read'
+    file_overwrite = False
