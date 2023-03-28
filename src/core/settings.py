d9a0beb5dc0c2ab5575cf5ef004b5a9f70889cf2
Festus Kwafo Ofori <68119922+Festus-Kwafo@users.noreply.github.com>
2023-03-28 13:43:16 +0000

    use s3 bool

--- src/core/settings.py
+++ src/core/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-USE_S3 = os.getenv('USE_S3') == 'TRUE'
+USE_S3 = os.getenv('USE_S3') == True
 
 if USE_S3:
     # aws settings
